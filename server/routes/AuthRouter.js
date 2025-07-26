09c80642c9260faaaa501de2ad743639016b620f
Dharmender <dharmender7077@gmail.com>
2025-07-26 10:10:24 +0530

    Add error logging for signup debugging

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -3,9 +3,6 @@ const { signupValidation, loginValidation } = require('../middleware/AuthValidat
 
 const router = require('express').Router();
 
-// Test route without validation
-router.post('/signup-test', signup);
-
 router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
